cc4b7b0b55af543c3395e40cf26cfe5b0cbc96a3
jonjon <jonjonclark@gmail.com>
2019-09-20 15:55:00 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Typist from 'react-typist';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  state = {
+    renderMsg: false,
+  }
+
+  onHeaderTyped = () => {
+    this.setState({ renderMsg: true });
+  }
+
+  render() {
+    const docs = 'https://wildcards.world';
+    return (
+      <div className="TypistExample">
+        <Typist
+          className="TypistExample-header"
+          avgTypingDelay={80}
+          startDelay={2000}
+          onTypingDone={this.onHeaderTyped}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <a href={docs}>alwaysforsale ad space</a>
+        </Typist>
+        <div className="TypistExample-content">
+          {this.state.renderMsg ? (
+            <Typist
+              className="TypistExample-message"
+              cursor={{ hideWhenDone: true }}
+            >
+              Rethink online advertising
+              <Typist.Delay ms={1250} />
+              <br />
+              <br/>
+              * Ethical advertising
+              <Typist.Delay ms={1250} />
+              <br />
+              * Set up in a matter of minutes
+              <Typist.Delay ms={1250} />
+              <br />
+              * Hard to recieve pay<Typist.Delay ms={500} />ments
+              <Typist.Backspace count={24} delay={1000} />
+              <Typist.Delay ms={750} />
+              Recieve ad reveunve directly to your crypto wallet
+              <Typist.Delay ms={1250} />
+              <br />
+              ... coming soon
+              <Typist.Delay ms={1250} />
+              <br />
+              <br />
+              <span>Meanwhile --> <a href={docs} className="flash">visit wildcards</a></span>
+              <br />
+
+              {''}
+            </Typist>
+          ) : null }
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
